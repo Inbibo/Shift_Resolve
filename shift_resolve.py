53da2ecc6cfb91542f1e8991ffa259fec6eaa93a
Jorge Hernandez <154216966+JorgeHernandezibanezinbibo@users.noreply.github.com>
2024-05-06 14:02:20 +0200

    Update shift_resolve.py
    Co-authored-by: Julia Visser <117650801+juliavisserinbibo@users.noreply.github.com>

--- shift_resolve.py
+++ shift_resolve.py
@@ -336,7 +336,6 @@ class DVR_FolderGet(DVR_Base):
         """Recursive function to find a folder object based in the full path of the folder.
         The Resolve API doesn't allow to get a folder by name or full path, only allows to get the
         root, current and subfolders. Because of this, this function is a utility to research over a given folder and subfolders
-
         to be able to return a folder given a full path from the root folder.
 
         @param currentFolder Resolve.Folder: The current folder to check the subFolders from.
